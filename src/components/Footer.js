9e9d6d35d156abe4e3ea1c93421b5269f43244c5
daveanderberg <daveanderberg@gmail.com>
2024-01-23 16:43:06 -0800

    Finished initial homepage styling and layout.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,44 @@
+import llFooterLogo from '../assets/llFooterLogo.png';
+import facebookLogo from '../assets/facebookLogo.png';
+import instagramLogo from '../assets/instagramLogo.png';
+import xLogo from '../assets/xLogo.png';
+
 function Footer() {
+    const imgStyle = {
+        filter: 'brightness(0) invert(1)',
+        height: '160px',
+    }
+
     return (
         <footer>
-            Footer.
+            <div>
+                <img src={llFooterLogo} style={imgStyle} />
+                <div>
+                    <h4>Doormat Navigation</h4>
+                    <ul>
+                        <li><a>Home</a></li>
+                        <li><a>About</a></li>
+                        <li><a>Menu</a></li>
+                        <li><a>Reservations</a></li>
+                        <li><a>Order Online</a></li>
+                        <li><a>Login</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h4>Contact</h4>
+                    <p>324 26th Street<br />Chicago, IL 97654</p>
+                    <br />
+                    <p><a href='tel:5099540290'>657-876-3241</a></p>
+                    <br />
+                    <p><a href='mailto:info@littlelemon.com'>info@littlelemon.com</a></p>
+                </div>
+                <div>
+                    <h4>Social Media</h4>
+                    <a href='http://www.facebook.com'><img src={facebookLogo} alt='Facebook logo' /></a>
+                    <a href='http://www.instagram.com'><img src={instagramLogo} alt='Instagram logo'/></a>
+                    <a href='http://www.twitter.com'><img src={xLogo} alt='X (formerly Twitter) logo'/></a>
+                </div>
+            </div>
         </footer>
     );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,22 +5,23 @@ function Header() {
         width: '20rem',
         height: '20rem',
         borderRadius: '16px',
-        margin: '20px 0',
         objectFit: 'cover',
     };
 
     return (
         <header>
             <div className="headerDiv">
-                <div>
+                <div className="headerTextColumn">
                     <h1>Little Lemon</h1>
                     <h2>Chicago</h2>
                     <p>
                         We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.
                     </p>
-                    <a href='' role='button'>Reserve a Table</a>
+                    <button className='yellowButton' id='reserveTableButton'>Reserve a Table</button>
                 </div>
-                <img src={llHeaderImage} style={imgStyle} alt="image of food" />
+                <div className="headerImageDiv">
+                    <img src={llHeaderImage} style={imgStyle} alt="image of food" />
+                </div>
             </div>
         </header>
     );
